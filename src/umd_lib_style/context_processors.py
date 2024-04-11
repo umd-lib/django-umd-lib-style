d6b9094577c2b6634ba75ea891790d7c8501d7c4
Peter Eichman <peichman@umd.edu>
2024-04-11 13:55:58 -0400

    LIBFCREPO-1319. Navigation links may now be defined per-request via a function.
    Also supports including unlinked text using a link name of "".
    
    https://umd-dit.atlassian.net/browse/LIBFCREPO-1319

--- src/umd_lib_style/context_processors.py
+++ src/umd_lib_style/context_processors.py
@@ -1,11 +1,25 @@
 import importlib.metadata
+from importlib import import_module
+from typing import Any
+
 from django.conf import settings
+from django.http import HttpRequest
 
 
-def app_info(_request):
+def app_info(request: HttpRequest) -> dict[str, Any]:
     package_name = getattr(settings, 'PROJECT_PACKAGE_NAME', None)
+
+    navigation_links = getattr(settings, 'NAVIGATION_LINKS', {})
+    if isinstance(navigation_links, str):
+        # this is a fully-qualified name to a function to run to get the navigation links
+        # this allows applications to customize the navigation links on a per-request basis
+        module_name, function_name = navigation_links.rsplit('.', 1)
+        module = import_module(module_name)
+        function = getattr(module, function_name)
+        navigation_links = function(request)
+
     return {
         'application_name': getattr(settings, 'APPLICATION_NAME', 'App'),
         'application_version': importlib.metadata.version(package_name) if package_name is not None else '',
-        'navigation_links': getattr(settings, 'NAVIGATION_LINKS', {}),
+        'navigation_links': navigation_links,
     }
